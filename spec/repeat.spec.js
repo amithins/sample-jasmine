11556b143458c14a64334dff1a12a24fa8e0a3b4
amithins <amith@cloudboxonline.com>
2015-07-03 14:42:39 +0530

    Update repeat.spec.js

--- spec/repeat.spec.js
+++ spec/repeat.spec.js
@@ -3,12 +3,11 @@ require("../src/repeat.js");
 describe("repeat", function() {
     it("repeats strings", function() {
         expect("abc".repeat(2)).toEqual("abcabc");
-//        expect("abc".repeat(0)).toEqual("");
     });
 
   	it("check equality",function(){
 		expect(isEqual(1,1)).toBe(true);
-//		expect(isEqual(1,2)).toBe(false);
+		expect(isEqual(1,2)).toBe(false);
 	});
 
 });
